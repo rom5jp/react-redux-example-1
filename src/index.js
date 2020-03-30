ce0b8c9173d08dc669bbcbc928caac2dd0474880
Romero Medeiros <romeromedeiros5@gmail.com>
2020-03-29 22:11:38 -0300

    Implements the Provider and a reducer to communicate with Field components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { combineReducers, createStore } from 'redux'
-import Provider from 'react-redux'
+import { Provider } from 'react-redux'
+import Field from './Field'
+
+const reducers = combineReducers(
+  {
+    field: () => ({value: 'Dumb component'})
+  }
+)
 
 ReactDOM.render(
-  <Provider >
-    <App />
-  </Provider>
-  ,document.getElementById('root')
-);
+  // Provider is the main and unique source of truth, it controls all the app states
+  // It can pass values to its components through the reducers
+  <Provider store={createStore(reducers)}> 
+    <Field initialValue='Initial Value' />
+  </Provider>,
+  
+  document.getElementById('root')
+)
