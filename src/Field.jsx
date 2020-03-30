33b38e138dc97b7f11d3c1ce5a026ae01a7c7da5
Romero Medeiros <romeromedeiros5@gmail.com>
2020-03-30 00:05:21 -0300

    Defines mapDispatchToProps(); Adds explanatory comments;

--- src/Field.jsx
+++ src/Field.jsx
@@ -1,19 +1,26 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import changeValue from './fieldActions'
 
 class Field extends Component {
   render() {
     return (
       <div>
         <label>{this.props.value}</label> <br/>
-        <input onChange={this.handleChange} value={this.props.value} />
+        <input onChange={this.props.changeValue} value={this.props.value} />
       </div>
     )
   }
 }
 
-const mapStateToProps = (state) => // ´state´ is the global state of the application providaded by the Provider component, through its attribute ´store´
-  ({ value: state.field.value }) // gets the 'value' from props, which is defined in the reducer, on the index.js file
+// ´state´ is the global state of the application provided by the Provider component that,
+// through its attribute ´store´, gets the 'value' from props, 
+// which is defined in 'fieldReducer' called from index.js file
+const mapStateToProps = (state) => ({ value: state.field.value }) 
 
-// the connect() method brings 'value' (defined in the reducer as ´field.value´) from the Store (Provider) in index.js
-export default connect(mapStateToProps)(Field)
+// Maps the action 'changeValue' implemented in the fieldActions.js, to the App Store (Provider component)
+const mapDispatchToProps = (dispatch) => bindActionCreators({ changeValue }, dispatch)
+
+// connect() brings 'value' (defined in the reducer as ´field.value´) from the Store (Provider) in index.js
+export default connect(mapStateToProps, mapDispatchToProps)(Field)
