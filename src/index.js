580d8b20726a329d8faf2131a0ace5a8dbb6708f
Romero Medeiros <romeromedeiros5@gmail.com>
2020-03-30 00:06:25 -0300

    Uses the fieldReducer in combineReducers()

--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ import ReactDOM from 'react-dom';
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 import Field from './Field'
+import fieldReducer from './fieldReducer'
 
+// reducers are objects of functions
 const reducers = combineReducers(
   {
-    field: () => ({value: 'Dumb component'})
+    field: fieldReducer // the 'field' property is controlled by the fieldReducer, which returns the function responsible to handle the changed value
   }
 )
 
 ReactDOM.render(
   // Provider is the main and unique source of truth, it controls all the app states
   // It can pass values to its components through the reducers
-  <Provider store={createStore(reducers)}> 
-    <Field initialValue='Initial Value' />
+  <Provider store={createStore(reducers)}>
+    <Field />
   </Provider>,
   
   document.getElementById('root')
